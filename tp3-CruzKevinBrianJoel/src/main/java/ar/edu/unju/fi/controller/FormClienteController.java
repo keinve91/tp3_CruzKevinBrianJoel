d60588132318b2917a427aaa8b12f56ef4d7408a
Joel Cruz <Joel Cruz@DESKTOP-ITJELBR>
2024-05-18 10:47:17 -0300

    Finalizacion del "FormClienteController"

--- tp3-CruzKevinBrianJoel/src/main/java/ar/edu/unju/fi/controller/FormClienteController.java
+++ tp3-CruzKevinBrianJoel/src/main/java/ar/edu/unju/fi/controller/FormClienteController.java
@@ -1,5 +1,13 @@
 package ar.edu.unju.fi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class FormClienteController {
-	
+@GetMapping("/formulario")
+public String getFormCliente() {
+    return "formCliente";
+}
 }
